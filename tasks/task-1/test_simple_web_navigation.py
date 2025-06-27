00f0a4bdce5f925a90d0497076e4d66dab08747b
Stian-M <140619566+Stian-M@users.noreply.github.com>
2025-06-27 15:43:06 +0200

    Update test_simple_web_navigation.py

--- tasks/task-1/test_simple_web_navigation.py
+++ tasks/task-1/test_simple_web_navigation.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright, expect
 
 # Task 1:
-# Open the swag labs website and check that the title "Swag Labs" is visible.
+# Open the swag labs website, saucedemo.com and check that the title "Swag Labs" is visible.
 # Try to do this first task without using playwright codegen
 
 def test_swag_labs_navigate():
